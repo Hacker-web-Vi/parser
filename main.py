5d58c661c8b1cc19fba029f8dda202d6e943d1cc
Hacker-web-Vi <mycryptobit99@gmail.com>
2025-01-04 00:05:26 +0100

    add genesis block to the first day

--- main.py
+++ main.py
@@ -307,6 +307,10 @@ async def parse_signatures_batches(validators,
                         logger.error(f"Failed to query valset at block {current_height}\nMake sure block range {start_height} --> {latest_height} is available on the RPC\nOr try to reduce blocks_batch_size size in config\nExiting")
                         exit()
 
+                    # TO INCLUDE GENESIS FIRST BLOCK INTO THE FIRST DAY
+                    if block['time'] == '2024-04-16':
+                        block['time'] = '2024-10-25'
+
                     if block['time'] not in day_boundaries:
                         day_boundaries[block['time']] = block['height']
 
